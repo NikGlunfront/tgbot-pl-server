969759fa957472faedaa79d3e7821eb6bd3e9b38
glunfront <glunfront@gmail.com>
2024-01-10 11:24:05 +0500

    upd_redis_conf

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
-const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'glunfront',
-  host: 'localhost',
-  database: 'gl_pl_db',
-  password: 'Nik132033',
-  port: 5432,
-})
+
+const PORT = 8000;
+
+const {pool} = require('./config/db')
 
 const token = '6572869925:AAHF10URck10LHvmeenurxj9Auc-JRzrUu0';
 const webAppUrl = 'https://flourishing-mermaid-7f2674.netlify.app';
@@ -18,6 +14,33 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+const redis = require("redis");
+
+// Connect to redis | async anonymous func
+(async () => {
+    client = redis.createClient();
+  
+    client.on("error", (error) => console.log('Что-то пошло не так', error)); // вешаем хук на ошибку подключения к серверу Redis
+  
+    await client.connect(); // подключаемся к серверу
+})();
+
+const onRequest = async (key, reqFunc) => {
+    let results; // заранее объявляем переменную для результата
+
+    const cacheData = await client.get(key); // пытаемся получить переменную post из базы данных Redis
+
+    if(cacheData) {
+        results = JSON.parse(cacheData); // парсим данные из формата сырой строки в формат структуры
+    } else {
+        results = await reqFunc(); // вызываем функцию получения данных с удаленного сервера
+    if(results.length === 0) throw "API error"; // обрабатываем пустой результат ошибкой
+        await client.set(key, JSON.stringify(results)); // кэшируем полученные данные
+    }
+
+    return results;
+}
 
 
 bot.on('message', async (msg) => {
@@ -68,8 +91,6 @@ bot.on('message', async (msg) => {
 
 });
 
-const PORT = 8000;
-app.listen(PORT, () => console.log('server started on port ' + PORT))
 
 app.post('/web-data', async (req, res) => {
     const {queryId, products, totalPrice} = req.body
@@ -94,11 +115,26 @@ app.post('/web-data', async (req, res) => {
     }
 })
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    let result
     pool.query('SELECT * FROM test_table', (error, results) => {
         if (error) {
             throw error
         }
-        res.status(200).json(results.rows)
+        result = results.rows
     })
+    let resValue = await client.set("first_rds_key", JSON.stringify(result)); // кэшируем полученные данные
+    res.status(200).json(resValue)
 })
+app.get('/getredis', async (req, res) => {
+    const cacheData = await client.get("first_rds_key"); // пытаемся получить переменную post из базы данных Redis
+    if (cacheData) {
+        res.status(200).json({cache_data: cacheData})
+        
+    } else {
+        res.status(500).json({error: 'error'})
+        
+    }
+})
+
+app.listen(PORT, () => console.log('server started on port ' + PORT))
